54e82207c5bcd513c3f6b62c1a928d11fcbbbfb3
David Renshaw <dwrenshaw@gmail.com>
2014-12-06 08:46:42 -0500

    use production environment

--- start.bash
+++ start.bash
@@ -6,14 +6,14 @@ export RBENV_ROOT=/usr/local/share/rbenv
 export PATH="$RBENV_ROOT/bin:$PATH"
 eval "$(rbenv init -)"
 
-rake db:schema:load
+RAILS_ENV=production rake db:schema:load
 
-cat | rails console <<__EOF__
+cat | rails console production <<__EOF__
 t = Tag.new
 t.tag = "test"
 t.save
 __EOF__
 
 
-rails server -p 10000
+rails server -p 10000 -e production
 
