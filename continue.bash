c5f9a55f6c204eca18aad2b58ce4ec7ad35e3e67
David Renshaw <dwrenshaw@gmail.com>
2015-01-24 15:11:00 -0500

    now expect to be run from lobsters-sandstorm

--- continue.bash
+++
@@ -1,6 +0,0 @@
-set -x
-
-export PATH="/usr/local/share/rbenv/versions/1.9.3-p429/bin:$PATH"
-
-./bin/rails server -p 10000 -e production
-
--- start.bash
+++
@@ -1,8 +0,0 @@
-set -x
-
-export PATH="/usr/local/share/rbenv/versions/1.9.3-p429/bin:$PATH"
-
-RAILS_ENV=production bundle exec rake db:schema:load populate_tags
-
-./bin/rails server -p 10000 -e production
-
