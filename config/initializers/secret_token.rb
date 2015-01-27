58687cb5c7151395f5bbe1d3a405a654f4c165be
David Renshaw <dwrenshaw@gmail.com>
2015-01-27 10:23:19 -0500

    generate secret token on startup and store in /var

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,1 +1,21 @@
-Lobsters::Application.config.secret_key_base = 'dccb76ad5df7fb6675150bcf55cfdf3ff0e4d50e2a14aebd872cd516c2e3ed9060ea8bb9f8d00b3747c698c13111e7a11c2ab1dec18393e598875d69aafb0f01'
+# taken from GitLab's secret_token.rb
+
+require 'securerandom'
+
+def find_secure_token
+  token_file = '/var/tmp/.secret'
+  if ENV.key?('SECRET_KEY_BASE')
+    ENV['SECRET_KEY_BASE']
+  elsif File.exist? token_file
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token of 64 random hexadecimal characters and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Lobsters::Application.config.secret_token = find_secure_token
+Lobsters::Application.config.secret_key_base = find_secure_token
