8d7e3eb5fedba817355d71d73bdf2fc496c99244
David Renshaw <dwrenshaw@gmail.com>
2015-01-28 12:15:30 -0500

    read-only-cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,10 @@ Lobsters::Application.configure do
   # Generate digests for assets URLs.
   config.assets.digest = true
 
+  config.assets.configure do |env|
+    env.cache = ActiveSupport::Cache::FileStore.new("read-only-cache/assets")
+  end
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
